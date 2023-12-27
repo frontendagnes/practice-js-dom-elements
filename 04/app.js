9f7e1b893a67c1bf110ac5497d423d5651087cb9
Agnieszka Kaminska <zabula81@o2.pl>
2023-12-27 14:27:43 +0100

    fourth

--- 04/app.js
+++ 04/app.js
@@ -6,3 +6,47 @@ const menuItems = [
     {text: 'galeria', url: '/gallery'},
     {text: 'kontakt', url: '/contact'},
 ];
+
+const navi = document.querySelector("nav")
+
+const ul = document.createElement("ul")
+
+navi.appendChild(ul)
+
+// const li = document.createElement("li")
+// const href = document.createElement("a")
+
+// href.href = "/"
+// href.innerText = "start"
+
+// const li1 = document.createElement("li");
+// const href1 = document.createElement("a");
+
+// href1.href = "/gallery";
+// href1.innerText = "galeria";
+
+// const li2 = document.createElement("li");
+// const href2 = document.createElement("a");
+
+// href2.href = "/constact";
+// href2.innerText = "kontakt";
+
+// li.appendChild(href)
+// li1.appendChild(href1)
+// li2.appendChild(href2);
+
+// ul.appendChild(li)
+// ul.appendChild(li1)
+// ul.appendChild(li2);
+
+
+menuItems.forEach((item) => {
+    const li = document.createElement("li");
+    const href = document.createElement("a");
+
+    href.href = item.url;
+    href.innerText = item.text;
+
+    li.appendChild(href);
+    ul.appendChild(li);
+})
