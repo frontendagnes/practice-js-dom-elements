cf76c9f809f55b453080248e45ebcb046a917467
Agnieszka Kaminska <zabula81@o2.pl>
2023-12-27 14:27:23 +0100

    third

--- 03/app.js
+++ 03/app.js
@@ -12,3 +12,19 @@ const buttonSettings = {
     },
     text: 'Click me!',
 }
+
+const parent = document.querySelector('.parent-for-button');
+const button = document.createElement('button')
+
+for(const prop in buttonSettings.attr){
+    button.setAttribute(prop, buttonSettings.attr[prop])
+}
+
+for (const prop in buttonSettings.css){
+    button.style[prop] = buttonSettings.css[prop]
+}
+
+button.innerText = buttonSettings.text
+
+console.log(button)
+parent.appendChild(button)
