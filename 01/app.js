3a571e075ffa6ab5559701b90e08abc4e6d62328
Agnieszka Kaminska <zabula81@o2.pl>
2023-12-27 14:26:25 +0100

    frist

--- 01/app.js
+++ 01/app.js
@@ -1,1 +1,10 @@
-console.log('DOM');
+console.log("DOM");
+
+const element = document.querySelector(
+  ".comments__item.comments__item--newest"
+);
+console.log("element>>", element);
+
+const data = element.querySelectorAll('[data-info]')
+
+console.log(data.length);
